edccb7c86b75b8a9b7e93714da1593a21eea742f
SaaketR <saaket2004@gmail.com>
2022-11-17 12:32:45 -0500

    Created Lesson 3 fiel

---
+++ Lesson_3.py
@@ -0,0 +1,22 @@
+'''
+Lesson 3 - Merge Sort, Quick Sort, and Divide and Conquer Algortihm
+
+Question:
+    You are working on a new feature on Jovian called "Top Notebooks of the Week". rite a function to sort a list of notebooks 
+    in decreasing order of likes. Keep in mind that up to millions of notebooks can be created every wweek, so your code has to be
+    efficiently as possible.
+
+Interpretation of the Question:
+    Write a program to sort a list of number in decreasing order.
+
+Test Cases:
+    1. [4, 2, 6, 3, 4, 6, 2, 1] (random order)
+    2. [5, 2, 6, 1, 23, 7, -12, 12, -243, 0] (random order)
+    3. [3, 5, 6, 8, 9, 10, 99] (already sorted)
+    4. [99, 10, 9, 8, 6, 5, 3] (already sorted, in decreasing order)
+    5. [5, -12, 2, 6, 1, 23, 7, 7, -12, 6, 12, 1, -243, 1, 0] (contains repeating elements)
+    6. [] (empty list)
+    7. [23] (list containing only one elemet)
+    8. [21,21,21,21,21] (list containing one element repeated multiple times)
+
+'''
