f15c0e1755f7b0cef28f2e608ea1e5a9ee37ccb1
SaaketR <saaket2004@gmail.com>
2022-11-17 13:32:59 -0500

    Added code for Insertion Sort

--- Lesson_3.py
+++ Lesson_3.py
@@ -19,4 +19,59 @@ Test Cases:
     7. [23] (list containing only one elemet)
     8. [21,21,21,21,21] (list containing one element repeated multiple times)
 
+Bubble Sort Algorithm:
+    1. Iterate over the array
+    2. Compare the current element with the number that follows it
+    3. If the following number is greater (or smaller, in case of ascending order) swap the two elements
+    4. Repeat, until array is sorted
+
+Insertion Sort Algorithm (source: https://www.geeksforgeeks.org/insertion-sort/)
+    1. Iterate over the array, starting from the second element
+    2. Compare the current element (key) with the preceeding elements
+    3. If the key element is greater (or smaller, in case of ascending order), swap the two elements, and repeat until the key is 
+    at the correct position
+    4. Repeat, until the array is sorted
 '''
+
+# Bubble Sort, descending order [Time = O(N^2); Space = O(N)]
+
+def bubble_sort(array):
+    arr = list(array)     # creating a copy of the array to not directly change the input array
+
+    i = 0
+    while (i < len(arr)-1):     # repeating the process n-1 times, where n is the length of the array
+        j = 0
+        while (j < len(arr)-1):     # performing the bubble sort
+            if (arr[j+1] > arr[j]):     # comparing and arranging in descending order
+                arr[j], arr[j+1] = arr[j+1], arr[j]         # swapping the numbers
+            j += 1
+        i += 1
+    
+    return arr
+
+# Insertion Sort, descending order [Time = O(N^2); Space = O(1)]
+
+def insertion_sort(array):
+    arr = list(array)       # creating a copy of the array to not directly change the input array
+
+    i = 1
+    while (i < len(arr)):       # iterating over the input array
+        key = arr[i]        # storing the current element into a variable (key)
+        
+        j = i-1
+        while ((j >= 0) and (key > arr[j])):        # conditions to find the appropriate position for the key
+            arr[j+1] = arr[j]       # pushing the preceeding element forward (swapping)
+            j -= 1
+        arr[j+1] = key      # appropriate position found, insert the key here
+
+        i += 1
+    return arr
+
+# Testing
+
+input = [4, 2, 6, 3, 4, 6, 2, 1]
+output = insertion_sort(input)
+
+
+print("Input: {}".format(input))
+print("Output: {}".format(output))
